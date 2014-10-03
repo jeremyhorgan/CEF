996e512493587e906e28c55d5f99178616adad77
jeremyhorgan <jeremyhorgan@hotmail.com>
2014-10-03 14:19:58 +0100

    Added CefHost

---
+++ CefHost/CefHost/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CefHost
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewDeveloperToolsMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(645, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewDeveloperToolsMenuItem});
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viewToolStripMenuItem.Text = "&View";
+            // 
+            // viewDeveloperToolsMenuItem
+            // 
+            this.viewDeveloperToolsMenuItem.Name = "viewDeveloperToolsMenuItem";
+            this.viewDeveloperToolsMenuItem.Size = new System.Drawing.Size(159, 22);
+            this.viewDeveloperToolsMenuItem.Text = "&Developer Tools";
+            this.viewDeveloperToolsMenuItem.Click += new System.EventHandler(this.viewDeveloperToolsMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(645, 290);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewDeveloperToolsMenuItem;
+    }
+}
+
---
+++ CefHost/CefHost/Form1.cs
@@ -0,0 +1,44 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace CefHost
+{
+    public partial class Form1 : Form
+    {
+        private WebView webView;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var browserSettings = new BrowserSettings();
+            browserSettings.FileAccessFromFileUrlsAllowed = true;
+            browserSettings.UniversalAccessFromFileUrlsAllowed = true;
+            browserSettings.TextAreaResizeDisabled = true;
+
+            webView = new WebView(Path.Combine(GetViewDirectory(), "mainpage.html"), browserSettings)
+            {
+                Dock = DockStyle.Fill
+            };
+
+            Controls.Add(webView);
+        }
+
+        private string GetViewDirectory()
+        {
+            return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().CodeBase), "View");
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {            
+        }
+
+        private void viewDeveloperToolsMenuItem_Click(object sender, System.EventArgs e)
+        {
+            webView.ShowDevTools();
+        }
+    }
+}
